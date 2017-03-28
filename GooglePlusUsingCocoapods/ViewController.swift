4a60d9290b3f07e160cb57ef91ec0a5d811382c3
pushpamgroupmobile <team.it@pushpamgroup.in>
2017-03-28 13:14:33 +0530

    fixed error which was making GIDUser object nil

--- GooglePlusUsingCocoapods/ViewController.swift
+++ GooglePlusUsingCocoapods/ViewController.swift
@@ -18,14 +18,12 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     @IBOutlet var statusLabel: UILabel!
     
     var gid_signinSharedInstance = GIDSignIn.sharedInstance()
-    
     var profileDataDict = [String: AnyObject]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // gid_signinSharedInstance?.clientID = "599280904951-9usr5as4773rkimklle3o6a08q3tui4i.apps.googleusercontent.com"
         gid_signinSharedInstance?.delegate = self
         gid_signinSharedInstance?.uiDelegate = self
         
@@ -36,7 +34,6 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     }
     
     //GIDSignInUIDelegate Methods
-    
     func sign(inWillDispatch signIn: GIDSignIn!, error: Error!){
         
         //UIActivityIndicatorView.stopAnimating()
@@ -55,10 +52,6 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     //GIDSignInDelegate Methods
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!){
         
-        print("GD DId Sign in, user : \(user)")
-        
-        
-        gid_signinSharedInstance?.scopes = ["https://www.googleapis.com/auth/plus.login","https://www.googleapis.com/auth/plus.me"]
         
         if ((gid_signinSharedInstance?.hasAuthInKeychain) != nil) {
             // Signed in
@@ -66,24 +59,6 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
             signOutButton.isHidden = false
             disconnectButton.isHidden = false
             statusLabel.text =  "logged In"
-            
-            
-            //let userId = user.userID as AnyObject // For client-side use only!
-            //            let idToken = user.authentication.idToken as String  // Safe to send to the server
-            //            let fullName = user.profile.name as String
-            //            let givenName = user.profile.givenName as String
-            //            let familyName = user.profile.familyName as String
-            //            let email = user.profile.email as String
-            //
-            //            if(user.profile.hasImage){
-            //
-            //                print("profile image available")
-            //
-            //            }
-            
-            
-            
-            
             
         } else {
             print("Not signed in")
@@ -93,63 +68,31 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
             statusLabel.text = "Google Sign in\niOS Demo"
         }
         
+        
         if (error == nil) {
             // Perform any operations on signed in user here.
             
-            print("error nil")
+            let userId :String = String(user.userID) as String// For client-side use only!
+            let idToken :String = user.authentication.idToken as String  // Safe to send to the server
+            let fullName:String = user.profile.name as String
+            let givenName: String = user.profile.givenName as String
+            let familyName: String = user.profile.familyName as String
+            let email: String = user.profile.email as String
             
-            let userId = user.userID as String // For client-side use only!
-            let idToken = user.authentication.idToken as String  // Safe to send to the server
-            let fullName = user.profile.name as String
-            let givenName = user.profile.givenName as String
-            let familyName = user.profile.familyName as String
-            let email = user.profile.email as String
+            //Add to Dict
+            profileDataDict = ["user_id":user.userID as AnyObject ,"id_token": user.authentication.idToken as AnyObject,"profile_name": user.profile.name as AnyObject,"given_name": user.profile.givenName as AnyObject,"family_name": user.profile.familyName as AnyObject,"email": user.profile.email as AnyObject, "profile_description": user.profile.description as AnyObject]
+            
+            print(profileDataDict)
             
             if(user.profile.hasImage){
-                
-                print("profile image available")
-                
+                print("profile image available")                
             }
-            
-            
-            
-            // ...
         } else {
             print("\(error.localizedDescription)")
         }
         
-        // profileDataDict = ["user_id":user.userID as AnyObject ,"id_token": user.authentication.idToken as AnyObject,"profile_name": user.profile.name as AnyObject,"given_name": user.profile.givenName as AnyObject,"family_name": user.profile.familyName as AnyObject,"email": user.profile.email as AnyObject, "profile_description": user.profile.description as AnyObject]
-        // print(profileDataDict)
-        
         
     }
-    //    private func finishAuthorizationForUser(signInUser: GIDGoogleUser) {
-    //
-    //        let servicePlus = GTLServicePlus()
-    //        servicePlus.authorizer = signInUser.authentication.fetcherAuthorizer()
-    //        servicePlus.fetchObjectWithURL(NSURL(string: "https://www.googleapis.com/plus/v1/people/me")!, completionHandler: { (ticket, object, error) -> Void in
-    //
-    //            guard error == nil else {
-    //                self.showUnsuccessfulLoginAlertWithMessage(error.description)
-    //                return
-    //            }
-    //
-    //            guard let user = object as? GTLObject else {
-    //                self.showUnsuccessfulLoginAlertWithMessage("Bad user")
-    //                return
-    //            }
-    //
-    //            let userJson = NSDictionary(dictionary: user.JSON) as! [String: AnyObject]
-    //
-    //            if let names = userJson["name"] as? [String: String] {
-    //                let lastName = names["familyName"]
-    //                let firstName = names["givenName"]
-    //                //...do something with these names
-    //            }
-    //        })
-    //
-    //    }
-    
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!){
         print("logged out")
         
